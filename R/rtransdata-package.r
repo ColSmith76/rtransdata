d40d05fd352c0b51dd59766ed423f2a65da2fead
ColSmith76 <colin76@gmail.com>
2014-06-01 21:20:46 -0400

    updating package documentation

--- R/rtransdata-package.r
+++ R/rtransdata-package.r
@@ -1,5 +1,11 @@
-#' rtransdata
+#' Download and analyze transportation data.
 #'
+#' @description
+#' A set of functions to work  with various public transportation data sources in R, 
+#' including producing tabulations, charts, and maps to show and analyze data. 
+#' This is primarily focused on US data sources, but includes functions that might be useful 
+#' for other data sources too.
+#' 
 #' @name rtransdata
 #' @docType package
 NULL
